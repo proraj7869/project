c85078639b27de9e5364c48205fd3114de651f2b
Raj Gandhi <rjgandhi159@gmail.com>
2024-11-16 13:47:11 +0530

    update

---
+++ Circuit/Q-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int rows = 5; 
+    int start = 41; 
+
+    // Outer loop for each row
+    for (int i = 0; i < rows; i++) 
+    {
+    
+        for (int j = 0; j <= i; j++) 
+        {
+            printf("%d ", start + j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
---
+++ Circuit/Q-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int rows = 4;
+    int num = 11;
+
+    for (int i = 1; i <= rows; i++) 
+    { 
+        for (int j = 1; j <= i; j++) 
+        { 
+            printf("%d ", num); 
+            num++; // Increment the number
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
